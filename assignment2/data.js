0a769276b245722b7bbd2e89f394c9f159b03f4e
def-ijk <huyd291102@gmail.com>
2024-09-30 02:34:17 +1000

    create index.html

---
+++ assignment2/data.js
@@ -0,0 +1,34 @@
+let songs = [
+  {
+    name: "Hes",
+    path: (src =
+      "https://thelongesthumstore.sgp1.cdn.digitaloceanspaces.com/IM-2250/p-hase_Hes.mp3"),
+    artist: "artist 1",
+    cover: (src =
+      "https://www.billboard.com/wp-content/uploads/2023/07/SZA-SOS-album-art-billboard-1240.jpg?w=800"),
+  },
+  {
+    name: "Dry down",
+    path: (src =
+      "https://thelongesthumstore.sgp1.cdn.digitaloceanspaces.com/IM-2250/p-hase_Dry-Down-feat-Ben-Snaath.mp3"),
+    artist: "artist 2",
+    cover: (src =
+      "https://design-assets.adobeprojectm.com/content/download/express/public/urn:aaid:sc:VA6C2:f7795bf8-f7f7-50fd-8986-b22afdbcf384/component?assetType=TEMPLATE&etag=4abb20fbfa864ee8af01d6c5f7dfbdd1&revision=694bd8b6-6a35-4cd6-aff9-47395d53b6b4&component_id=e74cd2c1-4360-4920-a56f-181d53b8dac1"),
+  },
+  {
+    name: "Leapt",
+    path: (src =
+      "https://thelongesthumstore.sgp1.cdn.digitaloceanspaces.com/IM-2250/p-hase_Leapt.mp3"),
+    artist: "artist 3",
+    cover: (src =
+      "https://i.pinimg.com/736x/a8/14/eb/a814eb4c51518adcf827f4ee64137f7c.jpg"),
+  },
+  {
+    name: "Water Feature",
+    path: (src =
+      "https://thelongesthumstore.sgp1.cdn.digitaloceanspaces.com/IM-2250/p-hase_Water-Feature.mp3"),
+    artist: "artist 4",
+    cover: (src =
+      "https://creativereview.imgix.net/content/uploads/2018/10/13.jpg?auto=compress,format&q=60&w=1200&h=1217"),
+  },
+];
